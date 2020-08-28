17690307064e22bb443d5bde7350e801b69c0f20
Daniel2005silva <daniel2005silva@gmail.com>
2020-08-28 14:34:41 -0300

    Adicionando comentario no código e alguns sites utilizados no README.md

--- README.md
+++ README.md
@@ -9,5 +9,18 @@ Teste para estágio
   
   No código, tendo os dados selecionados para a comparação, seria usado uma das funções de python para deixar o resultado mais preciso, e para a exibição nos graficos seria utilizado o grafico mais intuitivo possível para a visualização dos dados, provavelmente um grafico de linha, contendo além dos municipios selecionados a média dentre um total.
   
-  Todos os dados que já é mostrado para o munícipio individualmente daria para ser mostrado numa comparação, Ritmo de Contágio, Subnotificação, Capacidade Hospitalar, Isolamento Social,inclusive Casos Confirmados e Recuperados, com mais visibilidade assim como as 4 informações mais destacadas.
+  Todos os dados que já é mostrado para o munícipio individualmente daria para ser mostrado numa comparação, Ritmo de Contágio, Subnotificação, Capacidade Hospitalar, Isolamento Social, inclusive Casos Confirmados e Recuperados, com mais visibilidade assim como as 4 informações mais destacadas.
   
+
+
+REFERÊNCIAS ACESSADAS PARA SE FAMILIAZAR COM A LINGUAGEM
+
+	https://blog.4linux.com.br/web-scraping-python-selenium-e-beautifulsoup/   ("O mais usado")
+	https://blog.education-ecosystem.com/python-como-transformar-string-para-float-ou-int/
+	http://excript.com/python/if-else-python.html
+	https://pt.stackoverflow.com/questions/176243/como-limitar-n%C3%BAmeros-decimais-em-python
+	https://www.youtube.com/watch?v=Vxl5jUltHBo
+	.
+	.
+	.
+	("Dentre vários outros pesquisados")
--- app.py
+++ app.py
@@ -1,100 +1,80 @@
-# O modulo time aqui foi utilizado para esperar o carregamento das paginas atraves do firefox
+# Importação do modulo time para esperar o carregamento das paginas
 import time
-
-import pandas as pd
-# o modulo webdriver e necessario para definir qual navegador sera utilizado para fazer a automacao
+# Importação do modulo webdriver para definir qual navegador será utilizado
 from selenium import webdriver
-
+# Configuracao para permitir o funcionamento do webdriver
 from selenium.webdriver.chrome.options import Options
 chrome_options = Options()
 chrome_options.add_argument("--headless")
 chrome_options.add_argument('--no-sandbox')
 driver = webdriver.Chrome('/usr/lib/chromium-browser/chromedriver', options=chrome_options)
-# o modulo Select sera utilizado para interagir com a caixa de selecao onde sera definido o ano em que quero buscar os dados
-from selenium.webdriver.support.ui import Select
-
-# esse modulo sera utilizado para 
+# Modulo utilizado para selecionar as tags da pagina 
 from bs4 import BeautifulSoup
-
-# a linha abaixo define qual e o navegador que queremos utilizar, lembrando que eu instalei somente o driver para conexao com o firefox, mas existem tambem para Chrome e InternetExplorer
-#driver = webdriver.Chrome()
-
-# abaixo foi definido qual e o site que quero acessar
+# Método o site a ser acessado
 driver.get("https://dafbnafar-kb.saude.gov.br/s/desenvolvimento/app/kibana#/dashboard/dc5f0570-7f1f-11ea-ae22-c5191b41d130?embed=true&_g=()")
-
-#print(driver.page_source.encode('utf-8'))
-# o Sleep abaixo e para aguardar o carregamento da pagina
+# Método para aguardar o carregamento da pagina
 time.sleep(60)
-
-# Aqui estou buscando o elemento que possui na classe o valor valo01, que e respectivo ao valor da receita onde quero clicar para ir na proxima pagina
+# Atribuindo a variavel o botão na pagina. Método para encontrar o botão dá pagina pelo caminho completo no código
 btGrafico = driver.find_element_by_xpath("/html/body/div[2]/div/div/div/div[3]/dashboard-app/dashboard-viewport-provider/div/div/div[12]/div/div[1]/div/div/div/button")
-
-#print(receita)
+# Atribuindo a variavel o número total de exames no Brasil Positivo/Detectável
 tlBrasil = driver.find_element_by_xpath("/html/body/div[2]/div/div/div/div[3]/dashboard-app/dashboard-viewport-provider/div/div/div[9]/div/div[2]/div/div/div/div/div/div/div/span")
-# aqui e feito um clique no elemento que foi encontrado acima
+# Método para fazer o clique no botão armazenado na variavel
 btGrafico.click()
-
-# aguardando o carregamento da pagina
-#time.sleep(60)
-
+# Atribuindo a variavel o caminho do segundo botão para exibir a tabela
 btExibirTab = driver.find_element_by_xpath("/html/body/div[4]/div/div/div[2]/div/div/div[2]/button[1]")
-#print(btExibirTab)
-
+# Dando o clique nesse segundo botão encontrado
 btExibirTab.click()
-
-#time.sleep(30)
-
+# Atribuindo a varival o caminho do botão titulo da tabela para mudar a ordem de itens na tabela para Positivo/Detectável
 btMudarDados = driver.find_element_by_xpath("/html/body/div[4]/span/div/div/div[2]/div[3]/div[1]/table/thead/tr/th[2]/button")
-
-#print(btMudarDados)
-
+# Dando o clique para mudar a ordem de itens na tabela
 btMudarDados.click()
-
-#time.sleep(60)
-
+# Dando o segundo clique para deixar a tabela na ordem preterida
 btMudarDados.click()
-
-#time.sleep(60)
-# armazenando a div que possui a tabela dentro da variavel dados
+# Atribuindo a variavel o caminho da tabela
 dados = driver.find_element_by_xpath("/html/body/div[4]/span/div/div/div[2]/div[3]/div[1]/table")
-
-# aqui e pegado o codigo HTML que esta dentro da div bd01 no codigo que foi mostrado acima
+# Atribuindo a variavel o codigo HTML da tabela 
 html = dados.get_attribute("outerHTML")
+# Atribuindo a variavel o codigo HTML do número total de exames Positivo/Detectável no Brasil
 totalBr = tlBrasil.get_attribute("outerHTML")
-# com o codigo HTML dentro da variavel, vamos usar o BeautifulSoup para fazer o parser desse HTML
+# Método para converter o código
 soup = BeautifulSoup(html, "html.parser")
+# Atribuindo a variavel o código convertido em HTML
 posDetBr = BeautifulSoup(totalBr, "html.parser")
-# dentro da variavel soup temos o codigo html retornado pelo Selenium ja convertido para o BS
- # vou utilizar o metodo select_one para buscar o elemento table dentro desse codigo
-#table = soup.select_one("table")
+# Atribuindo a variavel a tabela
 tabela = soup.find(name='table')
-th = tabela.find_all('span', class_ = 'euiTableCellContent__text')
+# Selecionando todos th dentro da tabela
+#th = tabela.find_all('span', class_ = 'euiTableCellContent__text')
 
+# Declarações de vetores
 conteudo = []
 estados = []
 status = []
 tlExames = []
-titulo = []
+#titulo = []
 #icremento = 0
 #for i in th:
   #print(i.next_element)
   #titulo.append(i.next_element)
   #icremento+= 1
 
+# Atribuindo a variavel uma lista de td da tabela
 td = tabela.find_all('div', class_ = 'euiFlexItem euiFlexItem--flexGrowZero')
-
-
+# Método for para percorrer todos os td encontrados
 for c in td:
   #print(c.text)
+
+  # Armazenando no vetor um item por vez, convertido para texto
   conteudo.append(c.text)
 
+# Declarações de variaveis auxiliares para ajudar na obtenção dos itens desejados
 aux = 0
 aux1 = 0
 aux2 = 3
 aux3 = 6
 
 #print(conteudo)
-
+# Por haver tds em branco, e seguir o mesmo padrão de localização, lógica para ao percorrer a lista de td, separar quem é 
+# o estado, o resultado e o total
 for y in conteudo:
   if(aux == aux1):
     estados.append(y)
@@ -124,17 +104,17 @@ print("==============================================================")
 print("Total de resultados Positivo/Detectável no Brasil " + posDetBr.text)
 print("--------------------------------------------------------------")
 aux = 0
+# Método para percorrer os estados
 for w in estados:
   print(w + ":")
+  # tlExames armazenou o total de exames de cada estado
   print("Total de exames: " + tlExames[aux])
+  # str converte para string, round arredonda para duas casa decimais, float converte a string em número
   print(str(round((float(tlExames[aux]) / float(posDetBr.text) * 100), 2)) + "%")
   print("--------------------------------------------------------------")
   aux += 1
 
 '''
-df_full = pd.read_html(str(tabela))
-df = df_full[['UF de residência', 'Resultados', 'Nº de exames realizados']]
-df.columns = ['Estado', 'Situação', 'Total de exames']
 
 print(titulo[0])
 print(titulo[1])
